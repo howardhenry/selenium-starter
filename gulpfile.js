7f432364fe214d4b891579f83be3e45b940c75f2
Howard Henry <howard.henry@t4g.com>
2019-10-06 21:44:47 -0400

    chore: at utils dir to eslint config

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
 gulp.task('eslint', () => {
-  return gulp.src(['./tests/**/*.js', './gulpfile.js'])
+  return gulp.src(['./tests/**/*.js', './utils/**/*.js', './gulpfile.js'])
     .pipe(eslint())
     .pipe(eslint.format(''))
     .pipe(eslint.failAfterError());
