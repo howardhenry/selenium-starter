500091c4d2595afe3ac751f6035fe973239de846
Howard Henry <howard.henry@t4g.com>
2019-10-06 22:09:45 -0400

    docs: fix typo

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,7 +6,7 @@ docker-compose stop --timeout=0 && docker-compose rm -f
 # Start 5 instances of node-chrome - self-registers to Selenium Hub
 docker-compose up --force-recreate --detach --scale node-chrome=5 node-chrome
 
-# Build "test" container - triggers initialializing Selenium Hub
+# Build "test" container - triggers initializing Selenium Hub
 docker-compose build test
 docker-compose run test
 
